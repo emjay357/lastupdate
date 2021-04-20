80f54a594e8b6cb628e501969792b7989d05cc60
emjay357 <harynguyen357@gmail.com>
2021-04-21 05:03:47 +0700

    last update

---
+++ contactus.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="./css/styleneed.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/product.css">
+    <link rel="stylesheet" href="./css/contact.css">
+</head>
+<body>
+<?php
+ require "database.php";
+ $welcome = "";
+ if(isset($_GET['subscribe'])){
+    $sub = new product;
+    $result = $sub->subscribe($_GET['subscribe']);
+    if($result){
+        $welcome = "Thank you for subscribe";
+    }
+}
+ ?>
+    <div style="background-color: black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div>
+                        <h1><a href="./index.php">
+                            <img src="./image/logofix6.png" style="height: 65px; ">
+                        </a></h1>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="row border">
+                        <div class="search-box">
+                            <div>
+                                <form id="form-search" role="form" method="GET" action="./index.php">
+                                    <div>
+                                        <div class="input-group ml-4" style="width: 100%">
+                                            <input type="text" name="search" value="" id="search" class="form-control"
+                                                   placeholder="search">
+                                            <span class="input-group-btn">
+                                            <button id="btn-search" class="btn btn" style="background-color: rgb(254, 209, 1);">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+        </div>
+    </div>
+    </div>
+    <div id="navbar" class="mainmenu-area">
+        <div class="container">
+            <div class="row">
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a class="taskbar" href="./index.php">Home</a></li>
+                    </ul>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div>
+    <h2 id="contactheader">Contact us</h2>
+    </div>
+<center>
+    <div style="height: 30px;"></div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2214.636270032881!2d105.80342783844381!3d21.01029159344284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad010dd3488b%3A0x160b2950306a9d7c!2sEpro%20group%2C%20ecoding%20building!5e0!3m2!1svi!2s!4v1618735988894!5m2!1svi!2s" 
+        width="1150" height="400" style="border: 0px" allowfullscreen="" loading="lazy">
+    </iframe>
+    </center>
+    <div style="height: 30px;"></div>
+    <div class="footer-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 footeraboutus">
+                    <div class="footer-about-us">
+                        <h2>E<span>Pro</span></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt 
+                            id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, 
+                            modi aliquid eveniet eligendi iure at?</p>
+                    </div>
+                </div>
+                
+                
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">Categories</h2>
+                        <ul>
+                            <li><p>Fan</p></li>
+                            <li><p>Air conditioner</p></li>
+                            <li><p>Light bulb</p></li>
+                            <li><p>Fridge</p></li>
+                            <li><p>Microwave</p></li>
+                        </ul>                        
+                    </div>
+                </div>
+                <div class="subscribebox">
+                    <div>
+                        <h2 class="subheader">Newsletter</h2>
+                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+                        <div>
+                            <form action="./index.php">
+                                <input type="email" placeholder="Type your email">
+                                <input type="submit" value="Subscribe">
+                            </form>
+                            <br>
+                            <p><?php echo " ".$welcome; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p>&copy; 2021 ePro. All Rights Reserved.</p>
+                    </div>
+                </div>
+                
+                <div class="col-md-4">
+                    <div class="footer-card-icon">
+                    <i class="fa fa-cc-discover"></i>
+                        <i class="fa fa-cc-mastercard"></i>
+                        <i class="fa fa-cc-paypal"></i>
+                        <i class="fa fa-cc-visa"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
